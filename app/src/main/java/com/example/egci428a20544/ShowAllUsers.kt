9e0a814f4ebaf88cc6a6121f88b9b7d0e4c348e7
Jungs <>
2019-11-13 19:44:47 +0700

    Signin and ShowAllUser page template

---
+++ app/src/main/java/com/example/egci428a20544/ShowAllUsers.kt
@@ -0,0 +1,12 @@
+package com.example.egci428a20544
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowAllUsers : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_all_users)
+    }
+}
