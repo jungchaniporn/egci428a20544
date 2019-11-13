4f6b1b9c9c1a0d633f36ed5c45a6e6d736430ce3
Jungs <>
2019-11-13 17:46:27 +0700

    main and signup template

---
+++ app/src/main/java/com/example/egci428a20544/SignUp.kt
@@ -0,0 +1,14 @@
+package com.example.egci428a20544
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SignUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
